5f322bb4d3d19593d28c0bbeb6dfe7b04d022413
Clement Skau <clementskau@gmail.com>
2013-06-07 19:06:28 +0900

    .

--- py/editor_app.py
+++ py/editor_app.py
@@ -10,6 +10,84 @@ from PyQt4 import (
     )
 
 
+MODES = [
+  'ace/mode/abap',
+  'ace/mode/asciidoc',
+  'ace/mode/c9search',
+  'ace/mode/c_cpp',
+  'ace/mode/clojure',
+  'ace/mode/coffee',
+  'ace/mode/coldfusion',
+  'ace/mode/csharp',
+  'ace/mode/css',
+  'ace/mode/curly',
+  'ace/mode/dart',
+  'ace/mode/diff',
+  'ace/mode/django',
+  'ace/mode/dot',
+  'ace/mode/glsl',
+  'ace/mode/golang',
+  'ace/mode/groovy',
+  'ace/mode/haml',
+  'ace/mode/haxe',
+  'ace/mode/html',
+  'ace/mode/jade',
+  'ace/mode/java',
+  'ace/mode/javascript',
+  'ace/mode/json',
+  'ace/mode/jsp',
+  'ace/mode/jsx',
+  'ace/mode/latex',
+  'ace/mode/less',
+  'ace/mode/liquid',
+  'ace/mode/lisp',
+  'ace/mode/livescript',
+  'ace/mode/lua',
+  'ace/mode/luapage',
+  'ace/mode/lucene',
+  'ace/mode/makefile',
+  'ace/mode/markdown',
+  'ace/mode/objectivec',
+  'ace/mode/ocaml',
+  'ace/mode/perl',
+  'ace/mode/pgsql',
+  'ace/mode/php',
+  'ace/mode/powershell',
+  'ace/mode/python',
+  'ace/mode/r',
+  'ace/mode/rdoc',
+  'ace/mode/rhtml',
+  'ace/mode/ruby',
+  'ace/mode/scad',
+  'ace/mode/scala',
+  'ace/mode/scheme',
+  'ace/mode/scss',
+  'ace/mode/sh',
+  'ace/mode/sql',
+  'ace/mode/stylus',
+  'ace/mode/svg',
+  'ace/mode/tcl',
+  'ace/mode/tex',
+  'ace/mode/text',
+  'ace/mode/textile',
+  'ace/mode/tm_snippet',
+  'ace/mode/typescript',
+  'ace/mode/vbscript',
+  'ace/mode/xml',
+  'ace/mode/xquery',
+  'ace/mode/yaml',
+]
+
+EXT_MODE = {
+  '.py': 'ace/mode/python',
+  '.css': 'ace/mode/css',
+  '.svg': 'ace/mode/svg',
+  '.htm': 'ace/mode/html',
+  '.html': 'ace/mode/html',
+  '.js': 'ace/mode/javascript',
+}
+
+
 class FileDropZone(object):
   """Accept all file dropped on object and open URLs in new tabs"""
 
@@ -33,14 +111,23 @@ class FileDropZone(object):
 
 class WebBridge(QtCore.QObject):
   """Bridge object with javascript call-able python methods"""
+
+  state_change = QtCore.pyqtSignal()
+
   def __init__(self, parent=None, frame=None):
     super(WebBridge, self).__init__(parent)
 #    self.setObjectName("WebBridge")
     self.frame = frame
 
+  def call(self, func_args):
+    """Method for calling functions in JavaScript from Python"""
+    return self.frame.evaluateJavaScript(func_args)
+
 #  def _pyVersion(self):
 #    return sys.version
 #  pyVersion = QtCore.pyqtProperty(str, fget = _pyVersion)
+
+  # Python methods callable from JavaScript
 
 #  @QtCore.pyqtSignature('QString')
   @QtCore.pyqtSlot(str)
@@ -51,8 +138,39 @@ class WebBridge(QtCore.QObject):
   def open_file(self, path):
     return open(path).read()
 
-  def call(self, func_args):
-    self.frame.evaluateJavaScript(func_args)
+  @QtCore.pyqtSlot()
+  def signal_state_change(self):
+    self.state_change.emit()
+
+
+class StatusLabel(QtGui.QLabel):
+  """"""
+
+  choice = QtCore.pyqtSignal(str)
+
+  def __init__(self, label='', parent=None):
+    QtGui.QLabel.__init__(self, label, parent=parent)
+
+    self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+    self.connect(
+        self,
+        QtCore.SIGNAL('customContextMenuRequested(const QPoint&)'),
+        self.on_context_menu,
+        )
+
+    # Popup Menu
+    self.popMenu = QtGui.QMenu(self)
+#    self.popMenu.addSeparator()
+    for syntax in MODES:
+      action = self.popMenu.addAction(syntax)
+      self.connect(
+          action,
+          QtCore.SIGNAL('triggered()'),
+          lambda s=syntax: self.choice.emit(s),
+          )
+
+  def on_context_menu(self, point):
+    self.popMenu.exec_(self.mapToGlobal(point))
 
 
 class EditorWidget(QtWebKit.QWebView, FileDropZone):
@@ -88,12 +206,22 @@ class EditorWidget(QtWebKit.QWebView, FileDropZone):
     self.web_bridge.call(
         'openFile("{}")'.format(file_path),
         )
+    self.web_bridge.call(
+        'onCursorChange(WebBridge.signal_state_change)'
+        )
+    root, ext = os.path.splitext(file_path)
+    mode = 'ace/mode/text'
+    if ext in EXT_MODE:
+      mode = EXT_MODE[ext]
+    self.web_bridge.call(
+        'setMode(\'{}\')'.format(mode)
+        )
 
   def save(self):
     file_content = self.web_bridge.call("getContent()")
     file_path = self.web_bridge.call("getFilePath()")
     if file_path is None:
-      file_path = QtGui.QFileDialog.getSaveFileName(self)
+      file_path = QtGui.QFileDialog.getSaveFileName(None)
     if file_path:
       open(file_path, 'w').write(file_content)
 
@@ -134,10 +262,12 @@ class EditorApp(object):
 
     self.main_window.resize(800, 600)
 
-    self.label_position = QtGui.QLabel('')
-    self.label_spell = QtGui.QLabel('')
-    self.label_indent = QtGui.QLabel('')
-    self.label_highlight = QtGui.QLabel('')
+    self.label_position = StatusLabel()
+    self.label_spell = StatusLabel()
+    self.label_indent = StatusLabel()
+    self.label_highlight = StatusLabel()
+
+    self.label_highlight.choice.connect(self.change_syntax)
 
     # far left of first permanent, obscured by temp msg
 #    self.status_bar.addWidget(
@@ -253,12 +383,12 @@ class EditorApp(object):
 
     new_tab_index = self.tab_widget.addTab(new_editor_tab, tab_name)
     self.tab_widget.setCurrentIndex(new_tab_index)
-    
-    self.update_status_bar()
+
+    new_editor_tab.web_bridge.state_change.connect(self.update_status_bar)
 
   def open(self):
     file_name = QtGui.QFileDialog.getOpenFileName(
-        self.tab_widget,
+#        self.tab_widget,
 #        'Open File',
         )
     if not file_name:
@@ -275,11 +405,43 @@ class EditorApp(object):
 
   def update_status_bar(self):
 #    self.status_bar.showMessage('I\'m a status bar!')
-    if self.tab_widget.currentWidget():
-        cursor_position = self.tab_widget.currentWidget().web_bridge.call(
+    current_widget = self.tab_widget.currentWidget()
+    if current_widget:
+        cursor_position = current_widget.web_bridge.call(
             "getCursorPosition()"
             )
-        self.label_position.setText('')
-        self.label_spell.setText('')
-        self.label_indent.setText('')
-        self.label_highlight.setText('')
+        mode = current_widget.web_bridge.call(
+            "getMode()"
+            )
+        indent = current_widget.web_bridge.call(
+            "getIndent()"
+            )
+        if cursor_position:
+          self.label_position.setText(
+              'INSERT MODE, Line {}, Column {}'.format(
+                  int(cursor_position['row']) + 1,
+                  int(cursor_position['column']) + 1,
+                  )
+              )
+        self.label_spell.setText(
+            'Spelling who?'
+            )
+        if indent:
+          self.label_indent.setText(
+              (
+                  'Spaces: {}'
+                  if indent['useSoftTabs']
+                  else 'Tabs'
+                  ).format(
+                      int(indent['tabSize'])
+                      )
+              )
+        if mode:
+          self.label_highlight.setText(
+              mode
+              )
+
+  def change_syntax(self, choice):
+    self.tab_widget.currentWidget().web_bridge.call(
+        'setMode(\'{}\')'.format(choice)
+        )
